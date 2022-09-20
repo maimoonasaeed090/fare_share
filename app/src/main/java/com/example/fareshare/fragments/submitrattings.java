61f440f6eafb1809c2a9ac36a80443532f89d011
maimoonasaeed090 <113824875+maimoonasaeed090@users.noreply.github.com>
2022-09-20 20:19:21 +0500

    test git

--- app/src/main/java/com/example/fareshare/fragments/submitrattings.java
+++ app/src/main/java/com/example/fareshare/fragments/submitrattings.java
@@ -53,6 +53,7 @@ public class submitrattings extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            /*test git*/
         }
     }
 
